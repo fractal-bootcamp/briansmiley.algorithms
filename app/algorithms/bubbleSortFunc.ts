c72ba4d49bd114a3282015d4946bb3b89a518516
Brian Smiley <briantsmiley42@gmail.com>
2024-06-26 12:47:26 -0400

    adds comparisons to bubble sort history

--- app/algorithms/bubbleSortFunc.ts
+++ app/algorithms/bubbleSortFunc.ts
@@ -5,14 +5,14 @@ interface HistorySnapshot {
   comparisons: number;
   swaps: number;
 }
-const createHistorySnapshot = (
-  sorted: number[],
-  comparisons: number,
-  swaps: number
-) => ({ sorted: sorted, comparisons: comparisons, swaps: swaps });
+// const createHistorySnapshot = (
+//   sorted: number[],
+//   comparisons: number,
+//   swaps: number
+// ) => ({ sorted: sorted, comparisons: comparisons, swaps: swaps });
 const bubbleSort = (arr: number[]) => {
   const sorted = [...arr];
-  const comparisons = 0;
+  // const comparisons = 0;
   const history: number[][] = [[...sorted]];
   while (true) {
     const nextSwapSpot = sorted.findIndex(
@@ -34,5 +34,33 @@ const bubbleSort = (arr: number[]) => {
   }
 };
 
+interface BubbleHistory {
+  array: number[];
+  comparing: [number, number]; //indices of current comparison
+}
+export const bubbleSort2 = (arr: number[]) => {
+  const sorting = [...arr];
+  let sorted = false;
+  const history: BubbleHistory[] = [];
+  while (!sorted) {
+    let idx = 0;
+    sorted = true; //set sorted to true, to be reflagged false if we encounter any swaps
+    //go through whole array and make swaps
+    while (idx < sorting.length - 1) {
+      //log current comparison in history
+      history.push({
+        array: [...sorting],
+        comparing: [sorting[idx], sorting[idx + 1]]
+      });
+      //if element to the right is smaller, swap and set sorted to false
+      if (sorting[idx + 1] < sorting[idx]) {
+        [sorting[idx], sorting[idx + 1]] = [sorting[idx + 1], sorting[idx]];
+        sorted = false;
+      }
+      idx++;
+    }
+  }
+  return { originalArray: arr, sortedArray: sorting, history: history };
+};
 export default bubbleSort;
 console.log(bubbleSort([1, 5, 1, 2, 3, 1, 5, 16, 6, 34]));
--- app/components/BubbleSort.tsx
+++ app/components/BubbleSort.tsx
@@ -2,22 +2,29 @@
 
 import React, { use, useEffect, useState } from "react";
 import SortableBar from "./SortableBar";
-import bubbleSort from "../algorithms/bubbleSortFunc";
+import { bubbleSort2 } from "../algorithms/bubbleSortFunc";
 import { SortVizProps } from "./Sorts";
 
 export default function BubbleSort({ unsortedArray, tick }: SortVizProps) {
-  const solution = bubbleSort(unsortedArray);
+  const solution = bubbleSort2(unsortedArray);
   const [idx, setIdx] = useState(0);
   useEffect(() => {
     if (idx === solution.history.length - 1) return;
     setTimeout(() => setIdx(idx + 1), tick);
   }, [idx]);
+
   return (
     <div className="flex justify-start items-end">
-      {solution.history[idx].map(val => (
+      {solution.history[idx].array.map(val => (
         <div key={`bubbleSort${val}`}>
           <SortableBar
             height={val}
+            color={
+              solution.history[idx].comparing.includes(val) ||
+              idx === solution.history.length - 1
+                ? "green"
+                : undefined
+            }
             max={Math.max(...unsortedArray)}
             arrayLength={unsortedArray.length}
           />
