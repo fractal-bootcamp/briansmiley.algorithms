27dd373440d2824f5017e24179792869e7b66518
Brian Smiley <briantsmiley42@gmail.com>
2024-06-26 18:27:17 -0400

    removes toSorted method from scrambled

--- app/algorithms/utils.ts
+++ app/algorithms/utils.ts
@@ -1,5 +1,7 @@
-export const scrambled = (arr: number[]) =>
-  arr.toSorted((a, b) => Math.random() - 0.5);
+export const scrambled = (arr: number[]) => {
+  const ret = [...arr];
+  return ret.sort((a, b) => Math.random() - 0.5);
+};
 
 export const isSorted = (arr: number[]) =>
   arr.every((val, idx, arr) => idx < arr.length - 1 && val < arr[idx + 1]);
